d434fe54709fc070c4d78d2f75ff47b004db96a3
Vivian Zhu <tongyu@usc.edu>
2019-06-13 17:24:08 -0700

    add function in java

--- packages/expo-device-info/android/src/main/java/expo/modules/deviceinfo/DeviceInfoModule.java
+++ packages/expo-device-info/android/src/main/java/expo/modules/deviceinfo/DeviceInfoModule.java
@@ -3,6 +3,11 @@ package expo.modules.deviceinfo;
 import java.util.Map;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiInfo;
 
 import org.unimodules.core.ExportedModule;
 import org.unimodules.core.ModuleRegistry;
@@ -10,14 +15,19 @@ import org.unimodules.core.Promise;
 import org.unimodules.core.interfaces.ExpoMethod;
 import org.unimodules.core.interfaces.RegistryLifecycleListener;
 
+import java.net.InetAddress;
+
 public class DeviceInfoModule extends ExportedModule implements RegistryLifecycleListener {
   private static final String NAME = "ExpoDeviceInfo";
   private static final String TAG = DeviceInfoModule.class.getSimpleName();
 
   private ModuleRegistry mModuleRegistry;
+  private Context mContext;
+  private WifiInfo wifiInfo;
 
   public DeviceInfoModule(Context context) {
     super(context);
+    mContext = context;
   }
 
   @Override
@@ -30,7 +40,26 @@ public class DeviceInfoModule extends ExportedModule implements RegistryLifecycl
     mModuleRegistry = moduleRegistry;
   }
 
+  private WifiInfo getWifiInfo() {
+    if (this.wifiInfo == null) {
+      WifiManager manager = (WifiManager) mContext.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+      this.wifiInfo = manager.getConnectionInfo();
+    }
+    return this.wifiInfo;
+  }
+
   @ExpoMethod
-  public void someGreatMethodAsync(Map<String, Object> options, final Promise promise) {
+  public void getBatteryLevelAsync(Promise promise) {
+    Intent batteryIntent = mContext.getApplicationContext().registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+    int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    float batteryLevel = level / (float) scale;
+    promise.resolve(batteryLevel);
+  }
+
+  @ExpoMethod
+  public void getIPAddressAsync(Promise promise) {
+    String ipAddress = InetAddress.getHostAddress();
+    promise.resolve(ipAddress);
   }
 }
